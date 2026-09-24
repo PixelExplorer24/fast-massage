9ec69f8558502040a7f9cef9a329435c2b6eb03f
PixelExplorer24 <89674871+PixelExplorer24@users.noreply.github.com>
2026-09-25 01:35:51 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="fast-messenger-shell-v20";
-const RUNTIME="fast-messenger-runtime-v20";
+const CACHE="fast-messenger-shell-v21";
+const RUNTIME="fast-messenger-runtime-v21";
 const SHELL=["./","./index.html","./assets/js/app.js","./assets/theme-background.webp","./assets/logo.webp"];
 
 self.addEventListener("install",e=>e.waitUntil(
